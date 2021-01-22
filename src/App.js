78b8dcfc2928e8478c6f37d7137a271f58021ff2
ellie <ellie.tms1371@gmail.com>
2021-01-22 23:03:42 +0000

    add useStates and params

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React,{useEffect,useState} from "react"; 
-import TakeIngredients from "./components/TakeIngredients "; 
-import 'bootstrap/dist/css/bootstrap.css';
-
+import React, { useState, useEffect } from "react";
+import TakeIngredients from "./components/TakeIngredients";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
- 
+  const [data, setData] = useState();
+  const [firstIngredient, setFirstIngredient] = useState();
+  const [secondIngredient,setSecondIngredient] = useState();
+  const params = {
+    api_key = "UOH8WmlDML1ysY4YsLLc5QVwbT6ogaJZTV5Cj2ih",
+    query : `${firstIngredient}`
+
+  }
+  useEffect(() => {
+
+  }, [])
+
   return (
     <div className="pageContainer">
-    <TakeIngredients />
+      <TakeIngredients 
+      firstIngredient={firstIngredient}
+      secondIngredient={secondIngredient}
+      />
     </div>
   );
 }
--- src/components/TakeIngredients.js
+++ src/components/TakeIngredients.js
@@ -1,13 +1,19 @@
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
-function TakeIngredients() {
+function TakeIngredients({firstIngredient,secondIngredient}) {
+  return(
     <div className="container">
       <div className="row">
         <div className="col-10">
-          
+          <label for="firstIngredient">First-Ingredient : </label><br/>
+          <input type="text"  value={firstIngredient} id="firstIngredient"/><br/>
+          <label for="secondIngredient">Second-Ingredient : </label><br/>
+          <input type="text"  value={secondIngredient} id="secondIngredient"/>
         </div>
       </div>
     </div>
+  )
 }
 
 export default TakeIngredients;
