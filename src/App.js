2ffb587606fdbc92b189d1382865fdfce9665ff8
ellie <ellie.tms1371@gmail.com>
2021-01-23 22:12:55 +0000

    add states and onclick functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
 import TakeIngredients from "./components/TakeIngredients";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -8,20 +8,27 @@ function App() {
   const [secondIngredient,setSecondIngredient] = useState();
   const params = {
     api_key : "UOH8WmlDML1ysY4YsLLc5QVwbT6ogaJZTV5Cj2ih",
-    query : `${firstIngredient}`&`${secondIngredient}`,
+    query : `${firstIngredient}&${secondIngredient}`
   }
-  const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&query=${params.query}`
+  let url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(params.api_key)}&query=${params.query}`
   useEffect(() => {
-    fetch(api_url)
+    fetch(url)
     .then(response => setData(response.json()))
-    .then(console.log(data));
-  }, [])
-
+    .then(console.log("NEW DATA",data));
+  },[firstIngredient,secondIngredient])
+  const updateFirstIngredient = (event) => {
+    setFirstIngredient(event.target.value);
+  }
+  const updateSecondIngredient = (event) => {
+    setSecondIngredient(event.target.value)
+  }
   return (
     <div className="pageContainer">
       <TakeIngredients 
       firstIngredient={firstIngredient}
       secondIngredient={secondIngredient}
+      updateFirstIngredient={updateFirstIngredient}
+      updateSecondIngredient={updateSecondIngredient}
       />
     </div>
   );
--- src/components/TakeIngredients.js
+++ src/components/TakeIngredients.js
@@ -1,19 +1,37 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
-function TakeIngredients({firstIngredient,secondIngredient}) {
-  return(
+function TakeIngredients({
+  firstIngredient,
+  secondIngredient,
+  updateFirstIngredient,
+  updateSecondIngredient,
+}) {
+  return (
     <div className="container">
       <div className="row">
-        <div className="col-10">
-          <label for="firstIngredient">First-Ingredient : </label><br/>
-          <input type="text"  value={firstIngredient} id="firstIngredient"/><br/>
-          <label for="secondIngredient">Second-Ingredient : </label><br/>
-          <input type="text"  value={secondIngredient} id="secondIngredient"/>
+        <div className="col-12">
+          <label for="firstIngredient">First-Ingredient : </label>
+          <br />
+          <input
+            type="text"
+            onChange={(event) => {updateFirstIngredient(event);console.log(event.target.value)}}
+            value={firstIngredient}
+            id="firstIngredient"
+          />
+          <br />
+          <label for="secondIngredient">Second-Ingredient : </label>
+          <br />
+          <input
+            type="text"
+            onChange={(event) => {updateSecondIngredient(event);console.log(event.target.value)}}
+            value={secondIngredient}
+            id="secondIngredient"
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default TakeIngredients;
