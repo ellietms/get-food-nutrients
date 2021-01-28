a176e7abaa3aa66cd119fc9c5b7b4a5ceefc7614
ellie <ellie.tms1371@gmail.com>
2021-01-28 22:57:13 +0000

    add score for cards

--- src/components/IngredientInfo.js
+++ src/components/IngredientInfo.js
@@ -5,8 +5,14 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function IngredientInfo ({chosenIngredient,back}) {
     return(
-        <div>
+        <div className="container">
+          <div className="row">
+            <p>{chosenIngredient}</p>
+            <p>{chosenIngredient}</p> 
+            <p>{chosenIngredient}</p> 
+            <p>{chosenIngredient}</p>   
             <button onClick={() => back()}>Back</button>
+          </div>  
         </div>
     )
 }
