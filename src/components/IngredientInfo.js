e2c4232e3c955bee88bcfb254e0b5cbc5c5568bd
ellie <ellie.tms1371@gmail.com>
2021-01-28 23:09:54 +0000

    added info in ingredientInfo

--- src/components/IngredientInfo.js
+++ src/components/IngredientInfo.js
@@ -6,12 +6,18 @@ import "bootstrap/dist/css/bootstrap.css";
 function IngredientInfo ({chosenIngredient,back}) {
     return(
         <div className="container">
+         <button onClick={() => back()}>Back</button>
           <div className="row">
-            <p>{chosenIngredient}</p>
-            <p>{chosenIngredient}</p> 
-            <p>{chosenIngredient}</p> 
-            <p>{chosenIngredient}</p>   
-            <button onClick={() => back()}>Back</button>
+            <p>Description : {chosenIngredient.description}</p><br/>
+            <p>additionalDescriptions: {chosenIngredient.additionalDescriptions}</p><br/>
+            <p>Score: {Math.floor((chosenIngredient.score) / 5)}%</p><br/>
+            <p>foodNutrients: {chosenIngredient.foodNutrients.map(eachNutrient => 
+                <div>
+                    <p>nutrientName : {eachNutrient.nutrientName}</p>
+                    <p>value : {eachNutrient.value}</p>    
+                </div>    
+            )}
+           </p>   
           </div>  
         </div>
     )
