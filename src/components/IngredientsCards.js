8b2bb895377b0101c22def842d560e4359e4df46
ellie <ellie.tms1371@gmail.com>
2021-01-31 00:38:40 +0000

    add css for mainpage

--- src/components/IngredientsCards.js
+++ src/components/IngredientsCards.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import "../App.css";
 
 function IngredientsCards({ data , showInfo , setShowInfo , setChosenIngredient}) {
   let pageContents;
--- src/components/TakeIngredients.js
+++ src/components/TakeIngredients.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import "../App.css";
 
 function TakeIngredients({
   firstIngredient,
@@ -10,23 +11,23 @@ function TakeIngredients({
   return (
     <div className="container">
       <div className="row">
-        <div className="col-12">
-          <label for="firstIngredient">First-Ingredient : </label>
-          <br />
+        <div className="col-12 ingredientContainer">
+          <label for="firstIngredient" className="takeIngredient">First-Ingredient:</label>
           <input
             type="text"
             onChange={(event) => {updateFirstIngredient(event)}}
             value={firstIngredient}
             id="firstIngredient"
+            className="inputIngredient"
           />
           <br />
-          <label for="secondIngredient">Second-Ingredient : </label>
-          <br />
+          <label for="secondIngredient" className="takeIngredient">Second-Ingredient : </label>
           <input
             type="text"
             onChange={(event) => {updateSecondIngredient(event)}}
             value={secondIngredient}
             id="secondIngredient"
+            className="inputIngredient"
           />
         </div>
       </div>
