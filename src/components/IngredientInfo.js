2762618864b830d0523e9e3c681d4050e0885e6c
eli <ellie.tms1371@gmail.com>
2021-10-14 01:24:12 +0100

    updated code and css

--- src/components/IngredientInfo.js
+++ src/components/IngredientInfo.js
@@ -4,22 +4,22 @@ import "bootstrap/dist/css/bootstrap.css";
 
 
 function IngredientInfo ({chosenIngredient,back}) {
-    return(
-        <div className="container">
-         <button onClick={() => back()}>Back</button>
-          <div className="row">
-            <p>Description : {chosenIngredient.description}</p><br/>
-            <p>additionalDescriptions: {chosenIngredient.additionalDescriptions}</p><br/>
-            <p>Score: {Math.floor((chosenIngredient.score) / 5)}%</p><br/>
-            <p>foodNutrients: {chosenIngredient.foodNutrients.map(eachNutrient => 
-                <div>
-                    <p>nutrientName : {eachNutrient.nutrientName}</p>
-                    <p>value : {eachNutrient.value}</p>    
+    return( 
+          <div className="container"> 
+          <button className="backButton" onClick={() => back()}>Back</button> 
+          <div className="background">  
+            <div><p className="title">Description:</p><p className="item">{chosenIngredient.description}</p></div>
+            <div><p className="title">Additional descriptions:</p><p className="item">{chosenIngredient.additionalDescriptions}</p></div><br/>
+            <div><p className="title">Score:</p><p className="item">{Math.floor((chosenIngredient.score) / 5)}%</p></div><br/>
+            <div><p className="title">FoodNutrients:</p><p className="item">{chosenIngredient.foodNutrients.map(eachNutrient => 
+                <div className="foodNutrients">
+                    Nutrient: {eachNutrient.nutrientName} -- value : {eachNutrient.value}  
                 </div>    
             )}
-           </p>   
+            </p>
+           </div>   
           </div>  
-        </div>
+          </div>
     )
 }
 
